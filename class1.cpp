6d52870d3d4c9685f24ac7498237a981d54beee0
Shakib Uz Zaman <207609386+shakibuzzaman52@users.noreply.github.com>
2026-07-31 19:09:04 +0600

    Class 1

---
+++ class1.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string name;
+    cout<<"Enter your full name : ";
+    getline(cin, name);
+    cout<<name;
+
+    
+
+    return 0;
+}
