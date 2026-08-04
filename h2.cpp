ebb95990ee23a9a37d3502143835f0443e0ecb07
Shakib Uz Zaman <207609386+shakibuzzaman52@users.noreply.github.com>
2026-08-04 17:03:17 +0600

    h2 solved

--- h2.cpp
+++ h2.cpp
@@ -1,7 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 int main()
 {
-    
+    int a, b, c, x;
+    cin >> a >> b >> c >> x;
+
+    if (x <= a)
+    {
+        cout << fixed << setprecision(12) << 1.0 << endl;
+    }
+    else if (x > b)
+    {
+        cout << fixed << setprecision(12) << 0.0 << endl;
+    }
+    else
+    {
+        cout << fixed << setprecision(12) << (double)c / (b - a) << endl;
+    }
+
     return 0;
 }
