2aee6b6b041329dd99f9ef1b74d5eaa84a85bfbd
Shakib Uz Zaman <207609386+shakibuzzaman52@users.noreply.github.com>
2026-08-04 13:48:58 +0600

    c2 solved

---
+++ c2.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<"L";
+    for( int i = 1; i <= n; i++)
+    {
+        cout<<"o";
+    }
+    cout<<"ng"<<endl;
+    return 0;
+}
