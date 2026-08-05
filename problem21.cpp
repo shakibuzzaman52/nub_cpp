7f7ae117aefaf0804bc01097b7cc1da637a2ec33
Shakib Uz Zaman <207609386+shakibuzzaman52@users.noreply.github.com>
2026-08-05 15:31:16 +0600

    Class 1 & 2 All Problems solved

---
+++ problem21.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, x;
+    cin >> n >> x;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    bool found = false;
+    for (int i = 0; i < n; i++)
+    {
+        if (x == a[i])
+        {
+            found = true;
+            break;
+        }
+    }
+
+    if (found)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+    return 0;
+}
