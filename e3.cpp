00b5410e1ddfcab01d7a9caa23438e22d528e30e
Shakib Uz Zaman <207609386+shakibuzzaman52@users.noreply.github.com>
2026-08-09 01:02:31 +0600

    e3 solved

---
+++ e3.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+
+    vector<int> v;
+
+    for(int i = 0; i < s.size(); i++)
+    {
+        if(s[i] != '+')
+        {
+            v.push_back(s[i] - '0');
+        }
+    }
+
+    sort(v.begin(), v.end());
+
+    for(int i = 0; i < v.size(); i++)
+    {
+        if(i != 0)
+        {
+            cout << "+";
+        }
+
+        cout << v[i];
+    }
+
+    return 0;
+}
